afa499883dbb9c9f9909ed347c45ef3957f25d9a
Missael Gastelum <missaga@proton.me>
2023-11-30 10:43:20 -0600

    fix format keymap

--- lua/core/config/keymaps.lua
+++ lua/core/config/keymaps.lua
@@ -92,4 +92,4 @@ map('n', '<leader>fs', [[/<C-r><C-w><CR>]], { desc = '[F]ile [S]earch word in cu
 map('n', '<leader>fx', [[<cmd>!chmod +x %<CR>]], { desc = 'Grant current [F]ile e[X]ecution perm', silent = true })
 
 -- Format file without lsp
-map('n', '<leader>ff', [[gg=G<C-o><C-o>]], { desc = '[F]ormat [F]ile indent' })
+map('n', '<leader>ff', [[gg=G``zz]], { desc = '[F]ormat [F]ile indent' })
