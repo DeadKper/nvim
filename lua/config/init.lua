fc3ce731ed1716d6d8dde259d05c518a6cb97242
Missael Gatelum <35360914+DeadKper@users.noreply.github.com>
2024-03-03 09:02:41 -0700

    setup initial config

---
+++ lua/config/init.lua
@@ -0,0 +1,12 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- require('config.settings')
+require 'config.keymaps'
+-- if not vim.g.vscode then
+--   require('config.lazy')
+-- end
