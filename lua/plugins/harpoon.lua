dd4002bba1d023dfeed210ad80521a6f4d0fb538
Missael Gatelum <35360914+DeadKper@users.noreply.github.com>
2024-03-03 15:30:36 -0700

    add remaining config

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,34 @@
+return { -- harpoon
+  'ThePrimeagen/harpoon',
+  event = 'VimEnter',
+  branch = 'harpoon2',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  config = function()
+    local harpoon = require 'harpoon'
+
+    harpoon:setup()
+
+    vim.keymap.set('n', '<leader>ha', function()
+      harpoon:list():append()
+    end, { desc = '[H]arpoon [A]ppend' })
+    vim.keymap.set('n', '<leader>hf', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = '[H]arpoon [F]iles' })
+
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set('n', '<C-S-P>', function()
+      harpoon:list():prev()
+    end)
+    vim.keymap.set('n', '<C-S-N>', function()
+      harpoon:list():next()
+    end)
+
+    for i = 1, 10, 1 do
+      vim.keymap.set('n', '<leader>h' .. i % 10, function()
+        harpoon:list():select(i)
+      end)
+    end
+  end,
+}
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,28 @@
+return { -- Useful plugin to show you pending keybinds.
+  'folke/which-key.nvim',
+  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  config = function() -- This is the function that runs, AFTER loading
+    require('which-key').setup()
+
+    -- Document existing key chains
+    local mappings = {
+      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+      ['<leader>f'] = { name = '[F]iles', _ = 'which_key_ignore' },
+      ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+      ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
+      ['<leader>p'] = { name = '[P]royect', _ = 'which_key_ignore' },
+      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+    }
+
+    mappings['<leader>h#'] = { desc = '[H]arpoon jump to file [#]' }
+    for i = 1, 10, 1 do
+      ---@diagnostic disable-next-line: assign-type-mismatch
+      mappings['<leader>h' .. i % 10] = 'which_key_ignore'
+    end
+
+    require('which-key').register(mappings)
+  end,
+}
