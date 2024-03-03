fde3a403237e9c1d35150b29e0fca22cda5acf1a
Missael Gatelum <35360914+DeadKper@users.noreply.github.com>
2024-03-03 16:56:54 -0700

    move leader assignment

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,10 +1,3 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
 require 'config.settings'
 require 'config.keymaps'
 require 'config.autocmds'
