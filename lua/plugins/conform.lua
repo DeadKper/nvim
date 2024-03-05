a8115715b013bd97aa9c54613721455185b650f3
Missael Gatelum <35360914+DeadKper@users.noreply.github.com>
2024-03-04 17:05:14 -0700

    add fish_indent as a formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -12,6 +12,7 @@ return { -- Autoformat
       formatters_by_ft = {
         lua = { 'stylua' },
         sql = { 'sqlfmt' },
+        fish = { 'fish_indent' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
@@ -19,6 +20,11 @@ return { -- Autoformat
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
       },
+      formatters = {
+        fish_indent = {
+          command = 'fish_indent',
+        }
+      }
     }
     vim.keymap.set('n', '<leader>ff', function()
       conform.format { lsp_fallback = true, timeout_ms = 500 }
