02da8a71efd83deffc99589c83a1c87518db4642
Missael Gastelum <missaga@proton.me>
2024-03-07 11:29:22 -0600

    rename file

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,8 +3,8 @@ local icons = require 'icons'
 return {
   -- Set lualine as statusline
   'nvim-lualine/lualine.nvim',
-  event = 'VimEnter',
-  dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
+  event = 'BufWinEnter',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   -- See `:help lualine.txt`
   opts = {
     options = {
