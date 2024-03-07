1f6c47446817313074161f3f5e914d36e3c060b9
Missael Gastelum <missaga@proton.me>
2024-03-07 17:32:56 -0600

    fix sleuth autoindent

--- lua/plugins/sleuth.lua
+++ lua/plugins/sleuth.lua
@@ -1,27 +1,40 @@
 return {
   'tpope/vim-sleuth',
-  event = 'BufWinEnter',
   config = function()
     local indent = {
       valid = { 2, 4 },
       default = 4,
     }
 
-    -- Don't let sleuth set a tabstop of 8
-    vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufReadPost', 'BufFilePost', 'FileType' }, {
-      group = vim.api.nvim_create_augroup('sleuth-fix-indent', { clear = true }),
-      callback = function()
-        if vim.bo.expandtab then
-          if vim.tbl_contains(indent.valid, vim.bo.tabstop) then
-            vim.bo.tabstop = indent.default
-            vim.bo.shiftwidth = indent.default
-          end
-        else
-          if not vim.tbl_contains(indent.valid, vim.bo.shiftwidth) then
-            vim.bo.shiftwidth = indent.default
-          end
-          vim.bo.tabstop = vim.bo.shiftwidth
+    local function detect_indent()
+      vim.cmd [[silent Sleuth]]
+
+      if vim.bo.expandtab then
+        if vim.tbl_contains(indent.valid, vim.bo.tabstop) then
+          vim.bo.tabstop = indent.default
+          vim.bo.shiftwidth = indent.default
         end
+      else
+        if not vim.tbl_contains(indent.valid, vim.bo.shiftwidth) then
+          vim.bo.shiftwidth = indent.default
+        end
+        vim.bo.tabstop = vim.bo.shiftwidth
+      end
+    end
+
+    -- Set autocmd on BufWinEnter so it loads after sleuth
+    vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
+      once = true,
+      group = vim.api.nvim_create_augroup('sleuth-custom', { clear = true }),
+      callback = function()
+        -- Set initial indentation
+        detect_indent()
+
+        -- Don't let sleuth set a tabstop of 8
+        vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufReadPost', 'BufFilePost', 'FileType' }, {
+          group = vim.api.nvim_create_augroup('sleuth-custom', { clear = true }),
+          callback = detect_indent,
+        })
       end,
     })
   end
