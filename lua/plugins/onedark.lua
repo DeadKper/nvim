a1ceba6e161b41c71743dc5cff6da2659e68fa33
Missael Gatelum <35360914+DeadKper@users.noreply.github.com>
2024-03-09 16:48:12 -0700

    reverse: b0441e51b8f09877dc58f88e259875a0cdd60653

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -1,7 +1,7 @@
 return { -- Theme inspired by Atom
   'navarasu/onedark.nvim',
+  lazy = false,
   priority = 1000,
-  event = 'UIEnter',
   config = function()
     local onedark = require 'onedark'
 
