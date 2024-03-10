633f7f24f617cd7870915bf9989b39007aef5836
Missael Gatelum <35360914+DeadKper@users.noreply.github.com>
2024-03-09 20:03:49 -0700

    setup lua_ls and neodev, remove automatic_installation

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,6 +4,7 @@ return { -- Automatically install LSPs and related tools to stdpath for neovim
   dependencies = {
     'neovim/nvim-lspconfig', -- LSP configuration
     'williamboman/mason-lspconfig.nvim', -- Allow lspconfig integration to mason
+    'folke/neodev.nvim' -- Setup lua_ls to have better neovim integration
   },
   config = function ()
     -- Set new neovim capabilities granted by the LSP, requires 'hrsh7th/cmp-nvim-lsp' to be included
@@ -15,13 +16,36 @@ return { -- Automatically install LSPs and related tools to stdpath for neovim
     --  You can press `g?` for help in :Mason menu
     require('mason').setup()
 
+    -- Server configuration
+    local servers = {
+      lua_ls = {
+        settings = {
+          Lua = {
+            runtime = { version = 'LuaJIT' },
+            completion = {
+              callSnippet = 'Replace',
+            },
+          },
+        },
+      },
+    }
+
+    -- Specific neovim config folder
+    if vim.loop.cwd() == vim.fn.stdpath('config') then
+      servers.lua_ls.settings.Lua.workspace = {
+        checkThirdParty = false,
+        library = { vim.env.VIMRUNTIME },
+      }
+      require('neodev').setup()
+    end
+
     -- Setup mason lspconfig
     require('mason-lspconfig').setup {
-      automatic_installation = true,
+      ensure_installed = vim.tbl_keys(servers or {}), -- Install configured lsp servers
       handlers = {
         function(server_name)
           -- List of default servers and their configs
-          local server = {}
+          local server = servers[server_name] or {}
           -- This handles overriding only values explicitly passed by the server configuration above
           server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
           require('lspconfig')[server_name].setup(server)
