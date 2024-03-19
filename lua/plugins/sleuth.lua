7de5d92580ccf8507a6440e955f4caf6916f6947
Missael Gastelum <missaga@proton.me>
2024-03-19 09:40:39 -0600

    sleuth doesn't work sometimes with the way I handled it

--- lua/plugins/sleuth.lua
+++ lua/plugins/sleuth.lua
@@ -15,6 +15,16 @@ return {
     }
 
     local function set_indent(verbose)
+      if verbose then
+        local tabstop = tonumber(vim.api.nvim_exec2('set ts', { output = true }).output:match('tabstop=([0-9]+)'))
+        vim.bo.tabstop = 8 -- Make sure tabstop is 8 before Sleuth runs
+        local error = vim.api.nvim_exec2('Sleuth', { output = true }).output:match('Sleuth disabled.*')
+        if error then
+          print(error)
+          vim.bo.tabstop = tabstop
+          return
+        end
+      end
       local shiftwidth = tonumber(vim.api.nvim_exec2('set sw', { output = true }).output:match('shiftwidth=([0-9]+)'))
       local detected = shiftwidth ~= 8 and shiftwidth > 0
       if not detected then
@@ -54,26 +64,26 @@ return {
     vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufReadPost', 'BufFilePost', 'FileType' }, {
       group = vim.api.nvim_create_augroup('custom-sleuth', { clear = true }),
       callback = function()
-        local values = vim.api.nvim_exec2('set sw ts sts', { output = true }).output
-        local shiftwidth = tonumber(values:match('shiftwidth=([0-9]+)'))
-        local tabstop = tonumber(values:match('tabstop=([0-9]+)'))
-        local softtabstop = tonumber(values:match('softtabstop=(-?[0-9]+)'))
-        if softtabstop ~= -1 then
-          vim.bo.softtabstop = -1
-        end
-        if shiftwidth ~= 8 or tabstop ~= 8 then -- Indent already set
-          if shiftwidth ~= tabstop then
-            if vim.bo.expandtab then
-              vim.bo.tabstop = shiftwidth
-            else
-              vim.bo.shiftwidth = tabstop
-            end
-          end
-          return
-        end
         vim.defer_fn(function() -- Defer because it fails otherwise
+          local values = vim.api.nvim_exec2('set sw ts sts', { output = true }).output
+          local shiftwidth = tonumber(values:match('shiftwidth=([0-9]+)'))
+          local tabstop = tonumber(values:match('tabstop=([0-9]+)'))
+          local softtabstop = tonumber(values:match('softtabstop=(-?[0-9]+)'))
+          if softtabstop ~= -1 then
+            vim.bo.softtabstop = -1
+          end
+          if shiftwidth ~= 8 or tabstop ~= 8 then -- Indent already set
+            if shiftwidth ~= tabstop then
+              if vim.bo.expandtab then
+                vim.bo.tabstop = shiftwidth
+              else
+                vim.bo.shiftwidth = tabstop
+              end
+            end
+            return
+          end
           set_indent()
-        end, 50)
+        end, 100)
       end,
     })
   end,
