142d1347e7b2e85ef7e8f4885ccd69a6f10a17e9
DeadKper <missaga@proton.me>
2024-05-05 09:40:22 -0700

    ignore lualine on dap ui

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -12,6 +12,14 @@ return { -- Set lualine as statusline
         icons_enabled = true,
         component_separators = icons.lines.center,
         section_separators = '',
+        ignore_focus = {
+          'dapui_watches',
+          'dapui_breakpoints',
+          'dapui_scopes',
+          'dapui_console',
+          'dapui_stacks',
+          'dap-repl',
+        },
       },
       sections = {
         lualine_b = {
