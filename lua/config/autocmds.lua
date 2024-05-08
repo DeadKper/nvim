f13bc021c7b968795a48b77effed9e84f5b5d119
Missael Gastelum <missaga@proton.me>
2024-05-08 15:47:16 -0600

    set relative numbers when not insert mode

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,3 +5,17 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+local augroup_num_lines = vim.api.nvim_create_augroup('numbered-lines', { clear = true })
+vim.api.nvim_create_autocmd('InsertEnter', {
+  group = augroup_num_lines,
+  callback = function()
+    vim.opt.relativenumber = false
+  end,
+})
+vim.api.nvim_create_autocmd({ 'InsertLeave', 'UIEnter' }, {
+  group = augroup_num_lines,
+  callback = function()
+    vim.opt.relativenumber = true
+  end,
+})
