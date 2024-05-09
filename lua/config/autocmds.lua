86d32dc69bfb5c4106b6de478e34a2e46d5c9173
DeadKper <missaga@proton.me>
2024-05-08 22:04:34 -0700

    make autocmd for relative numbers smarter

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,15 +7,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 local augroup_num_lines = vim.api.nvim_create_augroup('numbered-lines', { clear = true })
-vim.api.nvim_create_autocmd('InsertEnter', {
+vim.api.nvim_create_autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter', 'WinLeave' }, {
   group = augroup_num_lines,
   callback = function()
-    vim.opt.relativenumber = false
+    if vim.opt.number then
+      vim.opt.relativenumber = false
+    end
   end,
 })
-vim.api.nvim_create_autocmd({ 'InsertLeave', 'UIEnter' }, {
+vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave', 'WinEnter' }, {
   group = augroup_num_lines,
   callback = function()
-    vim.opt.relativenumber = true
+    if vim.bo.filetype ~= 'dashboard' and vim.opt.number and vim.fn.mode() ~= 'i' then
+      vim.opt.relativenumber = true
+    end
   end,
 })
