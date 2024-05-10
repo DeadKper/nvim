3a56fd31bde6b4a49c20f38fe874663a0fc168b6
Missael Gastelum <missaga@proton.me>
2024-05-10 12:59:10 -0600

    change icon an hl group

--- lua/plugins/virt-column.lua
+++ lua/plugins/virt-column.lua
@@ -5,14 +5,13 @@ return { -- Comment with 'gc' in visual mode or block comment with 'gb', support
     local indent = require('config.indent')
     local virt_column = require('virt-column')
     virt_column.setup({
+      char = require('config.icons').lines.center,
       exclude = {
         filetypes = indent.get_exclude('colorcolumn', 'filetype'),
         buftypes = indent.get_exclude('colorcolumn', 'buftype'),
       },
-      highlight = 'ColorColumn',
     })
     indent.colorcolumn.auto_exclude = false
     indent.set_color_column()
-    vim.cmd.hi('ColorColumn guifg=' .. indent.colorcolumn.color .. ' guibg=NONE')
   end,
 }
