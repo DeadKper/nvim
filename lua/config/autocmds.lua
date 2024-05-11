9ce4c8abcc321723881e3c2e9ad10dcc6dae27bd
DeadKper <missaga@proton.me>
2024-05-11 13:32:34 -0700

    close some files with q

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -38,3 +38,27 @@ vim.api.nvim_create_autocmd({ 'BufEnter' }, {
     vim.opt_local.foldlevel = 99
   end,
 })
+
+-- Close some filetypes with <q>
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('close-with-q', { clear = true }),
+  pattern = {
+    'PlenaryTestPopup',
+    'help',
+    'lspinfo',
+    'notify',
+    'qf',
+    'query',
+    'spectre_panel',
+    'startuptime',
+    'tsplayground',
+    'neotest-output',
+    'checkhealth',
+    'neotest-summary',
+    'neotest-output-panel',
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = event.buf, silent = true })
+  end,
+})
