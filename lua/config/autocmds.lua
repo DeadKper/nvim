9c325dc459e1dd715e7b11c672367758a9a9a248
DeadKper <missaga@proton.me>
2024-05-11 13:37:29 -0700

    add reload check

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -83,3 +83,13 @@ vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
     vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
   end,
 })
+
+-- Check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd({ 'FocusGained', 'TermClose', 'TermLeave' }, {
+  group = vim.api.nvim_create_augroup('checktime', { clear = true }),
+  callback = function()
+    if vim.o.buftype ~= 'nofile' then
+      vim.cmd('checktime')
+    end
+  end,
+})
