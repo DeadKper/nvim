7cca1eadd1aa1753e09b8c43cf7ba83591580fa3
DeadKper <missaga@proton.me>
2024-05-10 18:12:04 -0700

    add yadm-git to fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,4 +1,8 @@
 return { -- Git integration in neovim
   'tpope/vim-fugitive',
+  dependencies = { 'seanbreckenridge/yadm-git.vim' },
   event = 'VeryLazy',
+  config = function()
+    vim.g.yadm_git_gitgutter_enabled = 0
+  end,
 }
