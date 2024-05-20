6f7a084eba16c5c99fe626875b7d02b653ca9e0b
Missael Gastelum <missaga@proton.me>
2024-05-20 17:54:25 -0600

    add animate

---
+++ lua/plugin/specs/mini/animate.lua
@@ -0,0 +1,18 @@
+return { -- Animations
+	"echasnovski/mini.animate",
+	event = "VeryLazy",
+	config = function()
+		local animate = require("mini.animate")
+		local max_time = 500
+		local scroll_time = 10
+		animate.setup({
+			cursor = {
+				timing = animate.gen_timing.linear({ duration = 10, unit = "step" }),
+			},
+			scroll = {
+				timing = animate.gen_timing.quartic({ duration = scroll_time, unit = "step", easing = "out" }),
+				subscroll = animate.gen_subscroll.equal({ max_output_steps = math.floor(max_time / scroll_time + 1) }),
+			},
+		})
+	end,
+}
