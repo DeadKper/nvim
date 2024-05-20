4ddbefcb6bdc68fedb0ff48ca6bdc389ff5e7047
Missael Gastelum <missaga@proton.me>
2024-05-20 12:46:45 -0600

    fix git signs not updating

--- lua/plugin/specs/lualine.lua
+++ lua/plugin/specs/lualine.lua
@@ -31,7 +31,19 @@ return { -- Set lualine as statusline
 				},
 				lualine_c = {
 					"filename",
-					"diff",
+					{
+						"diff",
+						source = function()
+							local gitsigns = vim.b.gitsigns_status_dict
+							if gitsigns then
+								return {
+									added = gitsigns.added,
+									modified = gitsigns.changed,
+									removed = gitsigns.removed,
+								}
+							end
+						end,
+					},
 				},
 				lualine_x = {
 					{ "diagnostics", symbols = icons.diagnostics },
