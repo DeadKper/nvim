b0aa74d4e5465d5c9df1c15e6fcabc15aaabfa3a
Missael Gastelum <missaga@proton.me>
2024-05-21 11:17:09 -0600

    enable neotree, just don't use it if don't need it

--- lua/plugin/specs/neotree.lua
+++ lua/plugin/specs/neotree.lua
@@ -1,7 +1,6 @@
 return { -- File tree on the side
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
-	cond = false,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
