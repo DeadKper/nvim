946fed435e7052f84397ad4da310652500b8e46a
DeadKper <missaga@proton.me>
2024-05-20 19:36:20 -0700

    add max steps

--- lua/plugin/specs/mini/animate.lua
+++ lua/plugin/specs/mini/animate.lua
@@ -4,7 +4,11 @@ return { -- Animations
 	config = function()
 		local animate = require("mini.animate")
 		local max_time = 500
-		local scroll_time = 10
+		local scroll_time = 7
+		local steps = math.floor(max_time / scroll_time)
+		if steps > 60 then
+			steps = 60
+		end
 		vim.g.mini_animate = 1
 		animate.setup({
 			cursor = {
@@ -12,7 +16,7 @@ return { -- Animations
 			},
 			scroll = {
 				timing = animate.gen_timing.quartic({ duration = scroll_time, unit = "step", easing = "out" }),
-				subscroll = animate.gen_subscroll.equal({ max_output_steps = math.floor(max_time / scroll_time + 1) }),
+				subscroll = animate.gen_subscroll.equal({ max_output_steps = steps }),
 			},
 		})
 	end,
