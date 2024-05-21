8b634a12a2677f4b5318143d7362da74c3cf6843
Missael Gastelum <missaga@proton.me>
2024-05-21 16:52:15 -0600

    show lsp status on notify

--- lua/plugin/specs/noice.lua
+++ lua/plugin/specs/noice.lua
@@ -10,11 +10,22 @@ return {
 		cmdline = {
 			view = "cmdline",
 		},
+		views = {
+			notify = {
+				replace = true,
+			},
+		},
 		lsp = {
 			override = {
 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 				["vim.lsp.util.stylize_markdown"] = true,
 				["cmp.entry.get_documentation"] = true,
+			},
+			progress = {
+				enabled = true,
+				format = "lsp_progress",
+				format_done = "lsp_progress_done",
+				view = "notify",
 			},
 		},
 		presets = {
