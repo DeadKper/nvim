ed97c26fa431ccb5e27940ace15986659c5fc488
Missael Gastelum <missaga@proton.me>
2024-05-21 13:30:47 -0600

    remove fidget, will use noice + notify

--- lua/plugin/specs/lspconfig.lua
+++ lua/plugin/specs/lspconfig.lua
@@ -1,9 +1,6 @@
 return { -- LSP configuration
 	"neovim/nvim-lspconfig",
 	lazy = true, -- Load this on mason
-	dependencies = {
-		{ "j-hui/fidget.nvim", opts = {} }, -- Useful status updates for LSP
-	},
 	config = function()
 		-- Set diagnostics icons
 		for name, icon in pairs(require("config.icons").diagnostics) do
