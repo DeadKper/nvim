8812f1f6d8fd0bfd10dc7ab70cdc0faa1270f570
Missael Gastelum <missaga@proton.me>
2024-05-21 18:28:03 -0600

    copy all attributes from hl groups

--- lua/plugin/specs/onedark.lua
+++ lua/plugin/specs/onedark.lua
@@ -17,13 +17,23 @@ return { -- Theme inspired by Atom
 
 		local function set_transparent_hl(hlgroups, fg)
 			for _, value in ipairs(hlgroups) do
-				local id = vim.fn.hlID(value)
+				local id = vim.fn.synIDtrans(vim.fn.hlID(value))
 				vim.api.nvim_set_hl(0, value, {
 					fg = fg or vim.fn.synIDattr(id, "fg#"),
-					sp = vim.fn.synIDattr(id, "sp#"),
 					bg = "NONE",
-					ctermfg = vim.fn.synIDattr(id, "ctermfg#"),
-					ctermbg = vim.fn.synIDattr(id, "ctermbg#"),
+					sp = vim.fn.synIDattr(id, "sp#"),
+					bold = vim.fn.synIDattr(id, "bold") == "1",
+					italic = vim.fn.synIDattr(id, "italic") == "1",
+					reverse = vim.fn.synIDattr(id, "reverse") == "1" or vim.fn.synIDattr(id, "inverse") == "1",
+					standout = vim.fn.synIDattr(id, "standout") == "1",
+					underline = vim.fn.synIDattr(id, "underline") == "1",
+					undercurl = vim.fn.synIDattr(id, "undercurl") == "1",
+					underdouble = vim.fn.synIDattr(id, "underdouble") == "1",
+					underdotted = vim.fn.synIDattr(id, "underdotted") == "1",
+					underdashed = vim.fn.synIDattr(id, "underdashed") == "1",
+					strikethrough = vim.fn.synIDattr(id, "strikethrough") == "1",
+					altfont = vim.fn.synIDattr(id, "altfont") == "1",
+					nocombine = vim.fn.synIDattr(id, "nocombine") == "1",
 				})
 			end
 		end
