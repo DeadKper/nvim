1d23bb757323b1a6dd3c2b0bb0772e3f91ea6a63
Missael Gastelum <missaga@proton.me>
2024-05-23 09:36:40 -0600

    configure leap

--- lua/plugin/specs/leap.lua
+++ lua/plugin/specs/leap.lua
@@ -1,8 +1,12 @@
 return { -- Jump to an occurence fast
 	"ggandor/leap.nvim",
+	event = "VeryLazy",
 	config = function()
 		local leap = require("leap")
-		leap.setup({})
+		leap.setup({
+			safe_labels = "",
+			labels = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM",
+		})
 		vim.keymap.set("n", "<leader>j", "<Plug>(leap)", { desc = "Leap in window" })
 		vim.keymap.set("n", "<leader>J", "<Plug>(leap-from-window)", { desc = "Leap from window" })
 	end,
