e70700d3538d1c9b295031417f80dfc2c5a7a6f6
DeadKper <missaga@proton.me>
2024-06-03 20:00:59 -0700

    revert: cfc5085e

--- lua/plugin/specs/sleuth.lua
+++ lua/plugin/specs/sleuth.lua
@@ -16,75 +16,47 @@ return { -- Auto detection for file indentation with custom logic in lua to add 
 		}
 
 		local function exec(cmd)
-			local ok, res = pcall(vim.api.nvim_exec2, cmd, { output = true })
-			if not ok then
-				return ""
-			end
-			return res.output or ""
+			return vim.api.nvim_exec2(cmd, { output = true }).output
 		end
 
-		local function set_indents(indent, spaces)
-			vim.bo.tabstop = indent
-			vim.bo.shiftwidth = indent
-			if spaces then
-				vim.bo.expandtab = spaces
-			end
-		end
-
-		local function detect_indent(echo)
-			if vim.b.custom_sleuth ~= nil and echo ~= true then
-				return
-			end
-
-			vim.b.custom_sleuth = true
-			local has_default = conf.filetype[vim.bo.filetype] ~= nil
-
-			if echo == true then
-				local cmd_text = exec("Sleuth")
-
-				if cmd_text:match("failed") and not has_default then
+		local function indent(sleuth)
+			if sleuth == true then
+				local text = exec("Sleuth")
+				if text:match("failed") then
 					vim.cmd("echohl WarningMsg")
-					vim.cmd("echo '" .. cmd_text .. "'")
+					vim.cmd("echo '" .. text .. "'")
 					vim.cmd("echohl NONE")
 					return
 				end
-			elseif not exec("verbose set ts sw"):match("sleuth[.]vim") and not has_default then
-				return
-			elseif vim.tbl_contains(conf.ignore, vim.bo.filetype) then
-				if echo == true then
-					vim.cmd("echohl WarningMsg")
-					vim.cmd("echo 'Cannot set indent for ignored filetype'")
-					vim.cmd("echohl NONE")
+			else
+				if not exec("verbose set ts sw"):match("sleuth[.]vim") then
+					return
 				end
-
-				return
 			end
-
-			local defaults = conf.filetype[vim.bo.filetype] or conf.default
 
 			local tabstop = vim.fn.eval("&ts")
 			local shiftwidth = vim.fn.eval("&sw")
-			local spaces = vim.fn.eval("&et") == 1
 
 			if shiftwidth == 0 or tabstop == shiftwidth then
-				if tabstop == 8 or tabstop == conf.default.indent then -- Sleuth didn't set anything, set defaults
-					set_indents(defaults.indent, defaults.spaces)
+				if (tabstop == 8 or tabstop == conf.default.indent) and not vim.tbl_contains(conf.ignore, vim.bo.filetype) then
+					local defaults = conf.filetype[vim.bo.filetype] or conf.default
+					vim.bo.tabstop = defaults.indent
+					vim.bo.shiftwidth = defaults.indent
+					vim.bo.expandtab = defaults.spaces
 				end
 			elseif tabstop < shiftwidth then
-				set_indents(tabstop)
+				vim.bo.shiftwidth = tabstop
 			else
-				if spaces then
-					set_indents(shiftwidth)
+				if vim.fn.eval("&et") == 1 then
+					vim.bo.tabstop = shiftwidth
 				end
 			end
 
-			spaces = vim.fn.eval("&et") == 1
-
-			if echo == true then
+			if sleuth == true then
 				print(
 					":setlocal "
-						.. (spaces and "et" or "noet")
-						.. (not spaces and " ts=" .. vim.fn.eval("&ts") or "")
+						.. (vim.fn.eval("&et") == 1 and "et" or "noet")
+						.. (vim.fn.eval("&et") == 0 and " ts=" .. vim.fn.eval("&ts") or "")
 						.. " sw="
 						.. vim.fn.eval("&sw")
 				)
@@ -92,29 +64,26 @@ return { -- Auto detection for file indentation with custom logic in lua to add 
 		end
 
 		vim.api.nvim_create_user_command("Indent", function()
-			detect_indent(true)
+			indent(true)
 		end, {})
 
 		local augroup = vim.api.nvim_create_augroup("indent", { clear = true })
 
-		vim.api.nvim_create_autocmd({ "BufAdd", "BufReadPost", "BufFilePost", "FileType" }, {
+		vim.api.nvim_create_autocmd({ "BufReadPost", "BufFilePost", "FileType" }, {
 			group = augroup,
-			callback = detect_indent,
+			callback = indent,
 		})
 
 		vim.api.nvim_create_autocmd({ "BufNewFile" }, {
 			group = augroup,
 			callback = function()
-				detect_indent()
+				indent()
 				vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 					group = augroup,
 					once = true,
 					buffer = vim.api.nvim_get_current_buf(),
 					callback = function()
-						vim.defer_fn(function()
-							vim.b.custom_sleuth = true
-							detect_indent()
-						end, 500)
+						vim.defer_fn(vim.cmd.Indent, 500)
 					end,
 				})
 			end,
