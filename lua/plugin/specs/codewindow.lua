e851240bd224f72b9bce115b8ec1dbd32a461079
DeadKper <missaga@proton.me>
2024-06-30 11:18:38 -0700

    change minimap for codewindows

---
+++ lua/plugin/specs/codewindow.lua
@@ -0,0 +1,16 @@
+return {
+	"gorbit99/codewindow.nvim",
+	cond = not vim.g.vscode,
+	config = function()
+		local codewindow = require("codewindow")
+		codewindow.setup({
+			auto_enable = true,
+			minimap_width = 10,
+			window_border = "none",
+			exclude_filetypes = { "help", "dashboard" },
+		})
+		vim.api.nvim_create_user_command("Minimap", function()
+			codewindow.toggle_minimap()
+		end, {})
+	end,
+}
