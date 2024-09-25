9810a325b5cc4d5574cd96a73d0fe901f2917512
Missael Gastelum <missaga@proton.me>
2024-09-24 18:10:22 -0700

    fix trouble keybinds

--- lua/plugin/specs/trouble.lua
+++ lua/plugin/specs/trouble.lua
@@ -10,38 +10,46 @@ return { -- List to show diagnostics, references, quickfixes and more
 			["<leader>t"] = { name = "[T]rouble", _ = "which_key_ignore" },
 		})
 
-		local trouble = require("trouble")
-		trouble.setup({
+		require("trouble").setup({
 			use_diagnostic_signs = true,
 		})
 
 		local ok, _ = pcall(require, "todo-comments")
 		if ok then
-			vim.keymap.set("n", "<leader>tc", [[:TodoTrouble<cr>]], { desc = "[T]rouble todo [C]omments", silent = true })
+			vim.keymap.set("n", "<leader>tc", ":TodoTrouble<cr>", { desc = "[T]rouble todo [C]omments", silent = true })
+		end
+
+		local last_mode = "Trouble diagnostics toggle"
+
+		local function toggle(str)
+			if str ~= nil then
+				str = "Trouble " .. str
+				return function()
+					last_mode = str
+					vim.cmd(str)
+				end
+			else
+				return function()
+					vim.cmd(last_mode)
+				end
+			end
 		end
 
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("trouble-lsp-attach", { clear = true }),
 			once = true,
 			callback = function()
-				vim.keymap.set("n", "<leader>tt", function()
-					trouble.toggle()
-				end, { desc = "[T]rouble [T]oggle" })
-				vim.keymap.set("n", "<leader>tw", function()
-					trouble.toggle("workspace_diagnostics")
-				end, { desc = "[T]rouble toggle [W]orkspace diagnostics" })
-				vim.keymap.set("n", "<leader>td", function()
-					trouble.toggle("document_diagnostics")
-				end, { desc = "[T]rouble toggle [D]ocument diagnostics" })
-				vim.keymap.set("n", "<leader>tq", function()
-					trouble.toggle("quickfix")
-				end, { desc = "[T]rouble toggle [Q]uickfix" })
-				vim.keymap.set("n", "<leader>tl", function()
-					trouble.toggle("loclist")
-				end, { desc = "[T]rouble toggle [L]ocation list" })
-				vim.keymap.set("n", "<leader>tr", function()
-					trouble.toggle("lsp_references")
-				end, { desc = "[T]rouble toggle LSP [R]eferences" })
+				vim.keymap.set("n", "<leader>tt", toggle(), { desc = "[T]rouble [T]oggle" })
+				vim.keymap.set(
+					"n",
+					"<leader>tb",
+					toggle("diagnostics toggle filter.buf=0"),
+					{ desc = "[T]rouble [B]uffer diagnostics" }
+				)
+				vim.keymap.set("n", "<leader>td", toggle("diagnostics toggle"), { desc = "[T]rouble [D]iagnostics" })
+				vim.keymap.set("n", "<leader>tq", toggle("qflist toggle"), { desc = "[T]rouble [Q]uickfix" })
+				vim.keymap.set("n", "<leader>tl", toggle("loclist toggle"), { desc = "[T]rouble [L]ocation list" })
+				vim.keymap.set("n", "<leader>tr", toggle("lsp toggle"), { desc = "[T]rouble LSP [R]eferences" })
 			end,
 		})
 	end,
