e9ed463422f79cf6e41bb910632d1d02311b2262
Missael Gastelum <missaga@proton.me>
2024-09-29 19:00:18 -0700

    hide deprecation warning

--- lua/plugin/specs/which-key.lua
+++ lua/plugin/specs/which-key.lua
@@ -1,5 +1,6 @@
 return { -- Useful plugin to show you pending keybinds
 	"folke/which-key.nvim",
+	version = "3.*", -- current mappings are deprecated so keep version 3 around until I fix them
 	event = "VeryLazy",
 	cond = not vim.g.vscode,
 	init = function()
@@ -16,6 +17,10 @@ return { -- Useful plugin to show you pending keybinds
 			["<leader>y"] = "which_key_ignore",
 			["<leader>f"] = { name = "[F]iles", _ = "which_key_ignore" },
 			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+		})
+
+		require("which-key").setup({
+			notify = false, -- ignore deprecation warning
 		})
 
 		wkconf.register()
