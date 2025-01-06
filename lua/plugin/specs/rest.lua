78a5ba68203b4440c5cd5f84704ffdf11e8c6740
Missael Gastelum <missaga@proton.me>
2025-01-06 09:08:31 -0700

    i'll deal with it later

--- lua/plugin/specs/rest.lua
+++ lua/plugin/specs/rest.lua
@@ -1,9 +1,10 @@
-return { -- Better explore
-	"rest-nvim/rest.nvim",
-	event = "VeryLazy",
-	cond = not vim.g.vscode,
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		"rest-nvim/tree-sitter-http",
-	},
-}
+return {}
+-- return { -- Better explore
+-- 	"rest-nvim/rest.nvim",
+-- 	event = "VeryLazy",
+-- 	cond = not vim.g.vscode,
+-- 	dependencies = {
+-- 		"nvim-treesitter/nvim-treesitter",
+-- 		"rest-nvim/tree-sitter-http",
+-- 	},
+-- }
