8b7f7f6b78ee0ccb051de5062bdd1af6cdaf2ff5
Missael Gastelum <missaga@proton.me>
2025-01-15 15:24:38 -0700

    add column toggle command

--- lua/config/general.lua
+++ lua/config/general.lua
@@ -8,3 +8,15 @@ vim.filetype.add({
 		j2 = "jinja",
 	},
 })
+
+vim.api.nvim_create_user_command("ToggleColumn", function()
+	if vim.fn.eval("&nu") == 1 then
+		vim.opt.signcolumn = "no"
+		vim.opt.number = false
+		vim.opt.relativenumber = false
+	else
+		vim.opt.signcolumn = "yes"
+		vim.opt.number = true
+		vim.opt.relativenumber = true
+	end
+end, {})
