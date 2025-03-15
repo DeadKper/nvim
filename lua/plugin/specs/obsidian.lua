e9771d61b50bd29a5aff4c0e4fed8b085697ab09
Missael Gastelum <missaga@proton.me>
2025-03-15 00:21:56 -0700

    make obsidian keybind it's own group

--- lua/plugin/specs/obsidian.lua
+++ lua/plugin/specs/obsidian.lua
@@ -17,7 +17,18 @@ return {
 				},
 			},
 		})
-		vim.keymap.set("n", "<leader>sf", vim.cmd.ObsidianQuickSwitch, { desc = "[S]earch [F]iles in obsidian.md" })
-		vim.keymap.set("n", "<leader>st", vim.cmd.ObsidianTags, { desc = "[S]earch [T]ags in obsidian.md" })
+
+		require("plugin.confs.which-key").add({
+			["<leader>o"] = { name = "[O]bsidian", _ = "which_key_ignore" },
+		})
+		vim.keymap.set("n", "<leader>oq", vim.cmd.ObsidianQuickSwitch, { desc = "[Q]uick Switch" })
+		vim.keymap.set("n", "<leader>ot", vim.cmd.ObsidianTags, { desc = "Search [T]ags" })
+		vim.keymap.set("n", "<leader>on", vim.cmd.ObsidianNew, { desc = "New note" })
+		vim.keymap.set("n", "<leader>oc", vim.cmd.ObsidianTOC, { desc = "Table of [C]ontents" })
+		vim.keymap.set("n", "<leader>os", vim.cmd.ObsidianSearch, { desc = "[S]earch" })
+		vim.keymap.set("n", "<leader>or", vim.cmd.ObsidianRename, { desc = "[R]ename current note" })
+		vim.keymap.set("n", "<leader>of", vim.cmd.ObsidianFollowLink, { desc = "[F]ollow Link" })
+		vim.keymap.set("n", "<leader>ob", vim.cmd.ObsidianBacklinks, { desc = "[B]acklinks" })
+		vim.keymap.set("n", "<leader>od", vim.cmd.ObsidianDailies, { desc = "[D]ailies" })
 	end,
 }
