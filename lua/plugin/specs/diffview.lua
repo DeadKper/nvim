48f8681a57822cfeeca0afe44c06720b9d2e6fcd
Missael Gastelum <missaga@proton.me>
2025-03-27 19:43:30 -0700

    config diffview

--- lua/plugin/specs/diffview.lua
+++ lua/plugin/specs/diffview.lua
@@ -4,5 +4,17 @@ return { -- File tree on the side
 	dependencies = {
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 	},
-	config = true,
+	config = {
+		view = {
+			merge_tool = {
+				layout = "diff3_mixed",
+			},
+		},
+		hooks = {
+			diff_buf_read = function(bufnr)
+				vim.opt_local.wrap = false
+				vim.opt_local.list = false
+			end,
+		},
+	},
 }
