68e25e50950dc4ccd3a7d3b0418ebfc5a08a1488
Missael Gastelum <missaga@proton.me>
2025-05-17 19:55:20 -0700

    move enable toggle

--- lua/core/masonconf.lua
+++ lua/core/masonconf.lua
@@ -3,9 +3,9 @@ local M = {}
 ---@class core.masonconf.package
 ---@field [1] string
 ---@field reqs nil|string|table<string>|table<table<string>>
+---@field enabled nil|boolean
 
 ---@class core.masonconf
----@field enabled nil|boolean
 ---@field ft string|table<string>
 ---@field lsp nil|string|core.masonconf.package
 ---@field formatter nil|string|core.masonconf.package
@@ -21,7 +21,7 @@ M.config = {
 	ansiblels = {
 		ft = "yaml",
 		lsp = { "ansible-language-server", reqs = { { "python", "python3" } } },
-		formatter = { "ansible-lint", reqs = { { "python", "python3" } } },
+		linter = { "ansible-lint", enabled = false, reqs = { { "python", "python3" } } },
 	},
 	lua_ls = {
 		ft = "lua",
@@ -115,46 +115,60 @@ function M.get_mason_packages(ft)
 	return packages
 end
 
+---@param enabled_only nil|boolean
 ---@return table<string, table<string>>
-function M.formatters_by_ft()
+function M.formatters_by_ft(enabled_only)
 	local formatters = {}
 	local filetypes = {}
-	local fmt
+	local formatter
 	for _, value in pairs(M.config) do
 		filetypes = value.ft ---@diagnostic disable-line: cast-local-type
 		if type(filetypes) == "string" then
 			filetypes = { filetypes }
 		end
 		for _, ft in ipairs(filetypes) do ---@diagnostic disable-line: param-type-mismatch
-			fmt = get_valid_packages(value.formatter)
-			if fmt ~= nil then
+			formatter = get_valid_packages(value.formatter)
+			if formatter ~= nil then
 				if formatters[ft] == nil then
 					formatters[ft] = {}
 				end
-				formatters[ft][#formatters[ft] + 1] = fmt
+				if enabled_only == nil or enabled_only then
+					if value.formatter.enabled == nil or value.formatter.enabled then
+						formatters[ft][#formatters[ft] + 1] = formatter
+					end
+				else
+					formatters[ft][#formatters[ft] + 1] = formatter
+				end
 			end
 		end
 	end
 	return formatters
 end
 
+---@param enabled_only nil|boolean
 ---@return table<string, table<string>>
-function M.linters_by_ft()
+function M.linters_by_ft(enabled_only)
 	local linters = {}
 	local filetypes = {}
-	local fmt
+	local linter
 	for _, value in pairs(M.config) do
 		filetypes = value.ft ---@diagnostic disable-line: cast-local-type
 		if type(filetypes) == "string" then
 			filetypes = { filetypes }
 		end
 		for _, ft in ipairs(filetypes) do ---@diagnostic disable-line: param-type-mismatch
-			fmt = get_valid_packages(value.linter)
-			if fmt ~= nil then
+			linter = get_valid_packages(value.linter)
+			if linter ~= nil then
 				if linters[ft] == nil then
 					linters[ft] = {}
 				end
-				linters[ft][#linters[ft] + 1] = fmt
+				if enabled_only == nil or enabled_only then
+					if value.linter.enabled == nil or value.linter.enabled then
+						linters[ft][#linters[ft] + 1] = linter
+					end
+				else
+					linters[ft][#linters[ft] + 1] = linter
+				end
 			end
 		end
 	end
@@ -179,7 +193,7 @@ end
 function M.enable_lsps()
 	local to_enable = {}
 	for key, value in pairs(M.config) do
-		if value.enabled == nil or value.enabled then
+		if value.lsp.enabled == nil or value.lsp.enabled then
 			to_enable[#to_enable + 1] = key
 		end
 	end
