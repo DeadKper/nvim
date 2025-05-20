59450c9856c4118a7b7c343308f74b039ab260c5
Missael Gastelum <missaga@proton.me>
2025-05-19 22:59:54 -0700

    add space to snippe

--- lua/luasnip/sh.lua
+++ lua/luasnip/sh.lua
@@ -7,6 +7,6 @@ local fmt = require("luasnip.extras.fmt").fmt
 return {
 	sn(
 		{ trig = "fun" },
-		fmt("function <>(){\n\t<>\n}", { i(1, "name"), i(0) }, { delimiters = "<>", indent_string = [[\t]] })
+		fmt("function <>() {\n\t<>\n}", { i(1, "name"), i(0) }, { delimiters = "<>", indent_string = [[\t]] })
 	),
 }
