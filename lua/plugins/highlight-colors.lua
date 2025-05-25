db62d03ef46fc4c3af1aad351bb0eb29b36f3231
Missael Gastelum <missaga@proton.me>
2025-05-24 23:03:42 -0700

    add nvim-highlight-colors and update plugins

---
+++ lua/plugins/highlight-colors.lua
@@ -0,0 +1,46 @@
+return {
+	"brenoprata10/nvim-highlight-colors",
+	dependencies = { "saghen/blink.cmp" }, -- for integration
+	event = "BufReadPre",
+	config = function()
+		local hlcolors = require("nvim-highlight-colors")
+		hlcolors.setup({})
+		require("blink.cmp").setup({
+			completion = {
+				menu = {
+					draw = {
+						components = {
+							-- customize the drawing of kind icons
+							kind_icon = {
+								text = function(ctx)
+									-- default kind icon
+									local icon = ctx.kind_icon
+									-- if LSP source, check for color derived from documentation
+									if ctx.item.source_name == "LSP" then
+										local color_item = hlcolors.format(ctx.item.documentation, { kind = ctx.kind })
+										if color_item and color_item.abbr ~= "" then
+											icon = color_item.abbr
+										end
+									end
+									return icon .. ctx.icon_gap
+								end,
+								highlight = function(ctx)
+									-- default highlight group
+									local highlight = "BlinkCmpKind" .. ctx.kind
+									-- if LSP source, check for color derived from documentation
+									if ctx.item.source_name == "LSP" then
+										local color_item = hlcolors.format(ctx.item.documentation, { kind = ctx.kind })
+										if color_item and color_item.abbr_hl_group then
+											highlight = color_item.abbr_hl_group
+										end
+									end
+									return highlight
+								end,
+							},
+						},
+					},
+				},
+			},
+		})
+	end,
+}
