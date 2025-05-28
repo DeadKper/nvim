e4f88031d962298f63f9462c446dda62d1c4c35b
Missael Gastelum <missaga@proton.me>
2025-05-28 09:24:09 -0700

    load oil.nvim faster

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,14 +1,14 @@
 return {
 	"stevearc/oil.nvim",
-	event = "VeryLazy",
+	event = "VimEnter",
 	dependencies = { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 	config = function()
 		require("oil").setup({ view_options = { show_hidden = true } })
 		vim.g.file_explorer = "Oil"
 		if vim.bo.filetype == "netrw" then
-			vim.defer_fn(function()
+			vim.schedule(function()
 				vim.cmd("Oil")
-			end, 50)
+			end)
 		end
 	end,
 }
